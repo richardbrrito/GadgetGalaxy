09516bc80d909c74fbf2779cab810b4cf193efc3
Raiyan Mahfuz Aulindo <aulindo.raiyan1107@gmail.com>
2023-10-06 15:25:45 -0400

    Deals Section half done. Header almost complete except search button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer"
+import Deals from "./Components/Deals/Deals"
 function App() {
   return (
     <div >
       <div>
         <div className="white-gradient">
           <Header/>
+          <Deals/>
+
           <Footer/>
         </div>
       </div>
