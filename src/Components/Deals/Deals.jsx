6c730aadfc63ffd3dce63c0ebcbb20f529403515
kyl-136 <kennethye2004@gmail.com>
2023-10-13 17:16:09 -0400

    Minor changes to images in deals

--- src/Components/Deals/Deals.jsx
+++ src/Components/Deals/Deals.jsx
@@ -11,7 +11,6 @@ const Deals = () => {
             <div className="paddings innerWidth d-container">
                 <div className="d-head flexColStart">
                     <span className = "orangeText">Deals</span>
-                    
                 </div>
                 <Swiper
                 slidesPerView={2}
