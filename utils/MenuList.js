f4454715630900885bc3ed57e8f3de7c596a7512
raihven <raihven@protonmail.com>
2023-10-23 20:35:12 -0400

    Update MenuList.js
    added all the laptops

--- utils/MenuList.js
+++ utils/MenuList.js
@@ -1,51 +1,117 @@
-import Headphones from '../assets/ProductImages/Headphones_1.png';
-import BoseHeadphones from '../assets/ProductImages/Headphones_2.png';
-import HPspectre from '../assets/ProductImages/Laptop_1.png';
-import ROGzephyrus from '../assets/ProductImages/Laptop_2.png';
-import SamsungFold from '../assets/ProductImages/Phone_1.png';
-import Iphone15 from '../assets/ProductImages/Phone_2.png';
+import Headphones from "../assets/ProductImages/Headphones_1.png";
+import BoseHeadphones from "../assets/ProductImages/Headphones_2.png";
+
+import SamsungFold from "../assets/ProductImages/Phone_1.png";
+import Iphone15 from "../assets/ProductImages/Phone_2.png";
+
+import HPspectre from "../assets/ProductImages/Laptop_1.png";
+import ROGzephyrus from "../assets/ProductImages/Laptop_2.png";
+import DellXPS from "../assets/ProductImages/Laptop_3.png";
+import AcerNitro from "../assets/ProductImages/Laptop_4.png";
+import AsusZenB from "../assets/ProductImages/Laptop_5.png";
+import MicroSurface from "../assets/ProductImages/Laptop_6.png";
+import HPeliteB from "../assets/ProductImages/Laptop_7.png";
+import HuaweiMB from "../assets/ProductImages/Laptop_8.png";
+import LenovoLegionP from "../assets/ProductImages/Laptop_2.png";
+import FramW16 from "../assets/ProductImages/Laptop_10.png";
 
 export const MenuList = [
   {
-    name: 'ASUS ROG Laptop',
-    price: '1400',
-    detail: 'Ram ',
+    name: "Framework 16",
+    price: "1200",
+    detail: "Modular",
+    image: FramW16,
+    type: "Laptop",
+  },
+  {
+    name: "Lenovo Legion Pro",
+    price: "1050",
+    detail: "16gb DDR4",
+    image: LenovoLegionP,
+    type: "Laptop",
+  },
+  {
+    name: "Huawei MateBook",
+    price: "1150",
+    detail: "2-in-1 Notebook",
+    image: HuaweiMB,
+    type: "Laptop",
+  },
+  {
+    name: "HP EliteBook",
+    price: "1500",
+    detail: "16gb DDR4",
+    image: HPeliteB,
+    type: "Laptop",
+  },
+  {
+    name: "Microsoft Surface",
+    price: "1145",
+    detail: '14.4" QHD Touchscreen',
+    image: MicroSurface,
+    type: "Laptop",
+  },
+  {
+    name: "Asus ZenBook",
+    price: "1200",
+    detail: "Intel Core i7",
+    image: AsusZenB,
+    type: "Laptop",
+  },
+  {
+    name: "ACER NITRO 5",
+    price: "1200",
+    detail: "15.6 inch",
+    image: AcerNitro,
+    type: "Laptop",
+  },
+  {
+    name: "Dell XPS 13",
+    price: "1700",
+    detail: "Tele 4k Touchscreen",
+    image: DellXPS,
+    type: "Laptop",
+  },
+  {
+    name: "ASUS ROG Laptop",
+    price: "1400",
+    detail: "Ram ",
     image: ROGzephyrus,
-    type: 'Laptop',
+    type: "Laptop",
   },
   {
-    name: 'iPhone 15',
-    price: '1000',
-    detail: 'New Camera',
-    image: Iphone15,
-    type: 'Phone',
-  },
-  {
-    name: 'Samsung Galaxy',
-    price: '900',
-    detail: 'Nice Phone',
-    image: SamsungFold,
-    type: 'Phone',
-  },
-  {
-    name: 'Headphones',
-    price: '50',
-    detail: 'Sounds Nice',
-    image: Headphones,
-    type: 'Accessory',
-  },
-  {
-    name: 'HP Spectre',
-    price: '985',
-    detail: 'RAM',
+    name: "HP Spectre",
+    price: "985",
+    detail: "RAM",
     image: HPspectre,
-    type: 'Laptop',
+    type: "Laptop",
   },
   {
-    name: 'Bose Headphone',
-    price: '245',
-    detail: 'Heavy Basse',
+    name: "iPhone 15",
+    price: "1000",
+    detail: "New Camera",
+    image: Iphone15,
+    type: "Phone",
+  },
+  {
+    name: "Samsung Galaxy",
+    price: "900",
+    detail: "Nice Phone",
+    image: SamsungFold,
+    type: "Phone",
+  },
+  {
+    name: "Headphones",
+    price: "50",
+    detail: "Sounds Nice",
+    image: Headphones,
+    type: "Accessory",
+  },
+  {
+    name: "Bose Headphone",
+    price: "245",
+    detail: "Heavy Basse",
     image: BoseHeadphones,
-    type: 'Accessory',
+    type: "Accessory",
   },
 ];
