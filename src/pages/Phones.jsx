e792b2bb78f6b57da389d4bfd569a933511d0184
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-10-23 20:31:46 -0400

    Update Phones.jsx
    updated to get from same css

--- src/pages/Phones.jsx
+++ src/pages/Phones.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { MenuList } from '../utils/MenuList';
 import MenuItem from '../Components/Products/MenuItem';
-import '../styles/Phones.css';
+import '../Components/Products/Menu.css';
 
 function Laptops() {
 
   const phoneItems = MenuList.filter(menuItem => menuItem.type === 'Phone');
   return (
-    <div className="LaptopContainer">
+    <div className="menu">
       <h1 className="menuTitle">Phones</h1>
 
       <div className="menuList">
