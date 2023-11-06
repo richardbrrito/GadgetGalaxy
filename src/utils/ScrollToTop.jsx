9f977fa5686804e2eeaaa0de7ef775d6e5f4f1b1
raihven <raihven@protonmail.com>
2023-11-06 18:50:11 -0500

    scrollToTop added minor chnges footer and header

--- src/utils/ScrollToTop.jsx
+++ src/utils/ScrollToTop.jsx
@@ -1,13 +1,48 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router";
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import '../styles/ScrollToTop.css';
 
-const ScrollToTop = (props) => {
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
   const location = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
 
-  return <>{props.children}</>
+  // Show button when page is scrolled upto given distance
+  const toggleVisibility = () => {
+    if (window.scrollY > 500) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Set the top coordinate to 0
+  // make scrolling smooth
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  useEffect(() => {
+    scrollToTop();
+  }, [location.pathname]);
+
+  return (
+    isVisible && (
+      <button onClick={scrollToTop} className="scroll-to-top">
+        ↑
+      </button>
+    )
+  );
 };
 
-export default ScrollToTop;
+export default ScrollToTopButton;
