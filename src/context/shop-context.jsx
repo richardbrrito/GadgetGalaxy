ba1860fa727e70eedcc3a0433a529cafe90f20ab
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-11-10 21:39:30 -0500

    Update shop-context.jsx
    added updatecartitem

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -15,6 +15,20 @@ export const ShopContextProvider = (props) =>{
   /*Define all states and pass the state to the provider which can be accessed in cart and shop */
   const[cartItems, setCartItems] = useState(getDefaultCart());
   
+  const getTotalCartAmount = () =>{
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item] > 0){
+        let itemInfo = MenuList.find((product) => product.id === Number(item))
+        totalAmount += cartItems[item] * itemInfo.price
+      }
+    }
+
+    return totalAmount;
+
+
+  }
+
   const addToCart= (itemId) =>{
     setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
   }
@@ -22,9 +36,13 @@ export const ShopContextProvider = (props) =>{
     setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
   }
 
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]: newAmount}));
+  };
 
 
-  const contextValue = {cartItems, addToCart, removeFromCart}
+
+  const contextValue = {cartItems, addToCart, removeFromCart, updateCartItemCount, getTotalCartAmount}
 
   return <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>;
   
