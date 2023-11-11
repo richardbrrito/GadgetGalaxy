dc9b3a48056691c5f5a16208c6c3f30ab7fb70ec
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-11-10 21:39:12 -0500

    Update cart-item.jsx
    added updatecartitem method

--- src/pages/cart-item.jsx
+++ src/pages/cart-item.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import MenuItem from '../utils/MenuItem';
+import '../styles/Cart.css';
+import { ShopContext } from '../context/shop-context';
+
 
 
 
@@ -7,6 +10,7 @@ import MenuItem from '../utils/MenuItem';
 
 export const CartItem = (props) => {
   const { id , name, price, detail, image } = props.data
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount} = useContext(ShopContext);
   return <div className="cartItem">
     <img src={image}/> 
     <div className="description">
@@ -17,6 +21,11 @@ export const CartItem = (props) => {
       <p>
         ${price}
       </p>
+      <div className="countHandler">
+        <button onClick={() => removeFromCart(id)}> - </button>
+        <input value ={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)}/>
+        <button onClick={() => addToCart(id)}> + </button>
+      </div>
     </div>
     {" "}
   </div>
