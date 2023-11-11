8853445b06b2902b7857e5cac10bea074aab00e8
Richard Brito <117929922+richardbrrito@users.noreply.github.com>
2023-11-10 21:38:25 -0500

    Update Cart.jsx
    added the cart menu and buttons

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,12 +2,16 @@ import React, {useContext} from "react";
 import MenuList from "../utils/MenuList.json";
 import { CartItem } from "./cart-item";
 import { ShopContext } from '../context/shop-context';
+import { useNavigate } from "react-router-dom";
 
 
 
 
 export const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount()
+
+  const navigate = userNavigate()
   return <div className="cart">
     <div>
     <h1 className="menuTitle"> Your Cart Items</h1>
@@ -19,7 +23,19 @@ export const Cart = () => {
           }
           return null; // Return null for products not in the cart
         })}
+        {totalAmount > 0 ? (
+
+         <div className="checkout">
+          
+          <p> Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate(<Products />)}> Continue Shopping </button>
+          <button> Checkout </button>
+        </div>
+          ) : (
+            <h1> Your Cart is Empty</h1>
+          )}
 
     </div>
   </div>
+  
 }
